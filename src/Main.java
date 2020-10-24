27483bbe64eec0872178a824abe50d82ba41f856
elif <elif>
2020-10-24 14:03:06 +0300

    updare main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+ 
+   public static void main(String[] args) {
+PrimeGenerator primeGenerator = new PrimeGenerator();
 
-    }
+System.out.println(primeGenerator.generatePrimes(50));
+}
+
 }
